45e998196d7abb588d66269872b6c7d7368c0e15
Nikita Kukushkin <kukushkin.n.a@yandex.ru>
2025-10-30 20:07:33 +0300

    Final_project_of_Sprint_5

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,4 +1,34 @@
-import junit.framework.TestCase;
-public class CatTest extends TestCase {
-  
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void testGetSound() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+
+        Cat cat = new Cat(feline);
+        List<String> actualFood = cat.getFood();
+
+        assertEquals(expectedFood, actualFood);
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+    }
 }
